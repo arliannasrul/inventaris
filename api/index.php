79dcb0ed63f7c596a6864822a81f421ee650ed84
arliannasrul <arliannasrul@gmail.com>
2026-06-18 14:39:31 +0700

    fix: Vercel routing strips /api prefix

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
+// Fix Laravel routing issue on Vercel: prevent stripping /api from URL
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 // Forward request to Laravel public/index.php for Vercel Serverless Functions
 require __DIR__ . '/../public/index.php';
